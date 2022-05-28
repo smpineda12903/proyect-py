d01a0893c95de7502eaeb5fd6a28b5ae5dddb9b9
mariana <marianaorozco27lopez@gmail.com>
2022-05-27 21:21:53 -0500

    Programacion

---
+++ Clases/TipoDeVehiculo.py
@@ -0,0 +1,24 @@
+
+class atencionalusuario:
+
+     def __init__(self, van: booleam, bus: booleam, microbus: booleam ):
+        self.van = van 
+        self.bus = bus
+        self.microbus = microbus
+        def Getvan (self):
+            # van
+            input ('van')
+            print(f'servicios del van')
+            def Getbus (self):
+             # bus
+             input ('bus')
+             print(f'servicios de bus')
+             #microbus
+             input('microbus')
+             print(f'servicios del microbus')
+
+
+
+
+
+
